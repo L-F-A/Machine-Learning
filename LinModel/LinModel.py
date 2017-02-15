8b104da7f8d4a39906ccbb28563819e3643f5557
L-F-A <lf.arsenault@gmail.com>
2017-02-14 20:40:08 -0500

    Update LinModel.py

--- LinModel/LinModel.py
+++ LinModel/LinModel.py
@@ -150,9 +150,9 @@ class RidgeReg:
 			#Using stochastic gradient descent found in NumMethos directory
                         from NumMethods import StochGrad
                         if self.Nbatch==1:
-                                w_0,ep,ite=StochGrad(self.__deriv_forSG,w_0,XX,y,self.Nlearn,(lam/self.Nlearn),lam/self.Nlearn,eta=self.eta,tol=self.tol,epochMax=self.epochMax,Nbatch=self.Nbatch)
+                                w_0,ep,ite,mess=StochGrad(self.__deriv_forSG,w_0,XX,y,self.Nlearn,(lam/self.Nlearn),lam/self.Nlearn,eta=self.eta,tol=self.tol,epochMax=self.epochMax,Nbatch=self.Nbatch)
                         else:
-                                w_0,ep,ite=StochGrad(self.__deriv,w_0,XX,y,self.Nlearn,(lam/self.Nlearn),lam/self.Nlearn,eta=self.eta,tol=self.tol,epochMax=self.epochMax,Nbatch=self.Nbatch)
+                                w_0,ep,ite,mess=StochGrad(self.__deriv,w_0,XX,y,self.Nlearn,(lam/self.Nlearn),lam/self.Nlearn,eta=self.eta,tol=self.tol,epochMax=self.epochMax,Nbatch=self.Nbatch)
                         self.w=w_0
                         self.epoch=ep
 
@@ -172,93 +172,3 @@ class RidgeReg:
                         return np.sum(ydiff**2,axis=0)/len(ytrue)
 
 #########################################################################################
-
-
-def ActLearnVar(Xt,lam,sig2,XA,type='InputX'):
-#########################################################################################
-# For Active learning in the context of ridge regression, returns an array with the new #
-# variances when diffent xt are tested. The columns of ones is already added in Xt      #            
-#                                                                                       #            
-# Xt   : matrix or vector containing the new points to be tested. Rows are the different#
-#        xt_i and columns, their features                                               #
-# lam  : Ridge regularization parameter                                                 #
-# sig2 : Noise of the data                                                              #
-# XA   : Either the matrix X of the training data (containing a column of ones) or      #
-#        A = X^TX the necessary matrix to start with                                    #
-# Type : Can be 'InputX' XA will be the matrix X with features inputs or 'InputA', XA   #
-#        will be the matrix A = X^TX already                                            #
-#                                                                                       #
-#########################################################################################
-
-
-	#New testing x
-	NXt=Xt.shape[0]
-	OneVec = np.ones((NXt,1))
-	XXt = np.concatenate((Xt, OneVec), axis=1)
-
-	pdim=XXt.shape[1]
-
-	#Data features
-	if type is 'InputX':
-		OneVec = np.ones((X.shape[0],1))
-        	XX = np.concatenate((X, OneVec), axis=1)
-		A=np.dot(XX.transpose(),XX)	
-	else:
-                A=XA.copy()
-
-        Imat = np.identity(pdim)
-        Imat[pdim-1][pdim-1] = 0.
-
-	if NXt == 1:
-		SIG_inv=lam*Imat + 1./sig2*A
-		if XXt.ndim != 1:
-                        XXt=XXt.reshape(XXt.shape[1],)
-                b=np.linalg.solve(SIG_inv,XXt)
-                sig02=np.array(sig2+np.dot(XXt,b))
-		id_pop=0
-		Ap=np.outer(XXt,XXt) + A
-        else:
-                sig02=0.
-		SIG_inv = lam*Imat + 1./sig2*A		
-
-                for r in range(NXt):
-
-                        b=np.linalg.solve(SIG_inv,XXt[r,:])
-                        sig02temp=sig2+np.dot(XXt[r,:],b)			
-			if sig02temp > sig02:
-				sig02=sig02temp
-				id_pop=r
-	
-		Ap=np.outer(XXt[id_pop,:],XXt[r,:]) + A 
-		
-	return id_pop,sig02,Ap
-
-#########################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
