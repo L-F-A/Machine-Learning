8e891383f7a81c1d7e331fbed72dd7e7876d3b9e
L-F-A <lf.arsenault@gmail.com>
2017-04-06 18:46:58 -0400

    Update bootsplit.py

--- SplittingData/bootsplit.py
+++ SplittingData/bootsplit.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-def BootSample(NB,Nn):
+def BootSample(NB,Nn,P=None):
 #########################################################################
 #	Generates NB bootstrap samples for training set of size Nn	#
 #########################################################################
 #	Inputs:								#
 #		NB     : Number of bootstrap samples			#
 #		Nn     : How many instances in the training set		#
+#		P      : If there is a probability vector		#
 #									#
 #	Outputs:							#
 #		sample : A list containing NB lists of Nn indices	#
@@ -14,9 +15,7 @@ def BootSample(NB,Nn):
 #########################################################################
 
 	sample=[]
-	
 	for b in range(NB):
-		sample.append(list(np.random.randint(0,Nn,size=Nn)))
-
+		sample.append(list(np.random.choice(Nn,size=Nn,p=P)))
 	return sample
 
