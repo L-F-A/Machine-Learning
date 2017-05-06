6f0d016f0a7cf2b752b228de1f22749dc5f115c2
Louis-Francois Arsenault <lf.arsenault@gmail.com>
2017-05-05 20:53:02 -0400

    Update Kernels.py

--- KernelModel/Kernels.py
+++ KernelModel/Kernels.py
@@ -5,7 +5,7 @@ def KernelCalc(X,Xt,Nl,Nt,var=None,typeK='Poly',typeD=None,T=False,xinterval=Non
 ##########################################################################################################################
 #                                                  Kernel		                                                 #
 #                                                                                                                        #
-#                           Louis-Francois Arsenault, Columbia University la2518@columbia.edu (2016)                     #
+#                   Louis-Francois Arsenault, Columbia University la2518@columbia.edu (2013-2017)                        #
 ##########################################################################################################################
 #                                                                                                                        #
 #       INPUTS:                                                                                                          #
@@ -14,7 +14,8 @@ def KernelCalc(X,Xt,Nl,Nt,var=None,typeK='Poly',typeD=None,T=False,xinterval=Non
 #    var   :  An array containing the hyperparameters of the choosen kernel, its #
 #             length will depend upon the specific kernel			 #
 #    typeK :  Which kernel : polynomial 'Poly', Gaussian 'Gau', Exponential 'Exp'# 
-#             or Matern 5/2 'Matern52' are possible				 #
+#             or Matern 5/2 'Matern52' and 'Gau_TriDiag' 			 #
+#	      (Gaussian Kernel with tri-diagonal covariance) are possible	 #
 #    typeD :  Which distance metric to use for non-poly kernel: Euclidean 'Euc', #
 #             Euclidean for continuous functions 'Euc_C' or Manhattan 'Man'	 #
 ##################################################################################
@@ -42,6 +43,21 @@ def KernelCalc(X,Xt,Nl,Nt,var=None,typeK='Poly',typeD=None,T=False,xinterval=Non
 			elif typeK == 'Matern52': 
                         	dij = np.sqrt(5.)*D/var[0]
                         	return (1.+ dij + (dij**2)/3.)*np.exp(-dij)
+			elif typeK == 'Gau_TriDiag':
+                                #var represent a tridiagonal pxp matrix
+                                #thus var[0:p] the diagonal, var[p:2*p-1] the lower diag and
+                                #the upper diagonal = lower diagonal
+                                p=Xt.shape[1]
+                                d=var[0:p]
+                                ld=var[p:2*p-1]
+                                U=sp.linalg.cholesky(np.diag(ld,-1)+np.diag(d,0)+np.diag(ld,1))
+                                K=np.zeros((Nt,Nl))
+                                for r in range(Nt):
+                                        DT=Xt[r,:]-X
+                                        eta=sp.linalg.solve_triangular(U,DT.T)
+                                        K[r,:]=np.exp(-0.5*np.einsum('ij,ji->i',eta.T,eta))
+
+                                return K
 #########################################################################################################################
 
 def KernelCalc_withD(D,var=None,typeK='Gau',sqEuc=False):
@@ -72,5 +88,23 @@ def KernelCalc_withD(D,var=None,typeK='Gau',sqEuc=False):
 	else:
 		print 'Kernel of type', typeK, ' is not implemented'
 #########################################################################################################################
+
+def KernelCalcDer_withD(D,var=None,typeK='Gau',sqEuc=False):
+#The first in return is the kernel, the second is the first derivative
+        if (typeK == 'Gau') and (sqEuc==True):
+                K=np.exp(-0.5*D/(var[0]**2))
+                return K,D*K/var[0]**3
+        elif (typeK == 'Gau') and (sqEuc==False):
+                K=np.exp(-0.5*D**2/(var[0]**2))
+                return K,D**2*K/var[0]**3
+        elif typeK == 'Exp':
+                K=np.exp(-D/var[0])
+                return K,D*K/var[0]**2
+        elif typeK == 'Matern52':
+                dij = np.sqrt(5.)*D/var[0]
+                K=(1.+ dij + (dij**2)/3.)*np.exp(-dij)
+                return K,-dij/var[0]*(1. + 2./3.*dij*np.exp(-dij) - dij**2/3.*np.exp(-dij))
+        else:
+                print 'Kernel of type', typeK, ' is not implemented'
 
 #Should add possibility to mix kenerls together like K1+K2, K1*K2 etc
